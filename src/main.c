d1e5b732bd272131e1900da2bc841118a39d825b
art@-v1r0s <arthurd2000@gmail.com>
2021-01-22 11:33:23 +0100

    base project

---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder;
+    GtkWidget       *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new_from_file("glade/window_main.glade");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
+}
+
+void inscription(GtkButton * button,GtkStack * stack){
+    
+    gtk_stack_get_visible_child_name(stack);
+    //gtk_stack_set_visible_child_name(stack,window);
+}
+void connexion(GtkButton * button,GtkLabel * label_title){
+
+    gtk_label_set_text(label_title, "ta cliquez connard");
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
+
