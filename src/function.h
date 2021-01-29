752317131e754d31020f90675e2acde7997e3c3d
art0-v1r0s <arthurd2000@gmail.com>
2021-01-29 14:49:45 +0100

    add file h
    structure app widget

---
+++ src/function.h
@@ -0,0 +1,10 @@
+
+typedef struct {
+    GtkWidget *entry_id_connexion;
+    GtkWidget *entry_pass_connexion;
+    GtkWidget *entry_id_inscription;
+    GtkWidget *entry_pass_inscription;
+    GtkWidget *rb_famille;
+    GtkWidget *rb_etudiant;
+    
+} app_widgets;
--- src/main.c
+++ src/main.c
@@ -1,14 +1,5 @@
 #include <gtk/gtk.h>
-
-typedef struct {
-    GtkWidget *entry_id_connexion;
-    GtkWidget *entry_pass_connexion;
-    GtkWidget *entry_id_inscription;
-    GtkWidget *entry_pass_inscription;
-    GtkWidget *rb_famille;
-    GtkWidget *rb_etudiant;
-    
-} app_widgets;
+#include"function.h"
 
 
 
